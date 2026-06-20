91c61a40ad55d420d347f47ba32a901dca1d71fe
Sebastian Gonzalez Rossi <sebastian.gonzalez06@alumnos.ucn.cl>
2026-06-20 11:33:07 -0400

    Version 0.0.3 -En coleccion se agrega ordenar carta por nombre -En coleccion se agrega ordenar carta por rareza

--- src/Gui/PanelColeccion.java
+++ src/Gui/PanelColeccion.java
@@ -1,4 +1,5 @@
 package Gui;
+import java.awt.BorderLayout;
 import java.util.ArrayList;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
@@ -7,7 +8,7 @@ import Logic.*;
 
 public class PanelColeccion extends JPanel {
 
-	
+	private JComboBox<String> comboOrden; //Haremos una combobox para ordenar
 	private JTable tabla; // Tabla visual que verá el usuario
 	private DefaultTableModel modelo; // Modelo que almacena los datos de la tabla
 	private ISistema sistema = App.sistema; // Referencia al Singleton del sistema
@@ -15,6 +16,13 @@ public class PanelColeccion extends JPanel {
 	public PanelColeccion() {
 
 		modelo = new DefaultTableModel(); // Creamos un modelo vacío
+		
+		comboOrden = new JComboBox<>();
+		comboOrden.addItem("Nombre");
+		comboOrden.addItem("Rareza");
+		comboOrden.addItem("Poder");
+		add(comboOrden, BorderLayout.NORTH);
+		comboOrden.addActionListener(e -> actualizarTabla());
 
 		// Agregamos las columnas de la tabla
 		modelo.addColumn("Nombre");
@@ -33,12 +41,39 @@ public class PanelColeccion extends JPanel {
 	}
 	
 	private void cargarCartas() {
-		
-		ArrayList<Carta> cartas = sistema.getCartas(); // Obtenemos el ArrayList de cartas desde el sistema
 
-		for(Carta c : cartas) {
-			modelo.addRow(new Object[] {c.getNombreCarta(),c.getRareza(),c.getTipo()});
+		ArrayList<Carta> cartas = sistema.getCartas();// Obtenemos la lista de cartas del sistema
+
+		String opcion = comboOrden.getSelectedItem().toString();// Obtenemos la opción seleccionada en el ComboBox
+
+		if(opcion.equals("Nombre")) { // Ordenar por nombre (A-Z)
+
+			cartas.sort(
+				(c1, c2) ->
+				c1.getNombreCarta().compareToIgnoreCase(c2.getNombreCarta())
+			);
 
 		}
+
+		// Ordenar por rareza (Mayor a menor)
+		else if(opcion.equals("Rareza")) {cartas.sort((c1, c2) ->Integer.compare(c2.getRareza(), c1.getRareza()));
+
+		}
+
+		else if(opcion.equals("Poder")) {// Aquí haremos el Visitor más adelante
+
+		}
+
+		for(Carta c : cartas) {
+
+			modelo.addRow(new Object[] {c.getNombreCarta(),c.getRareza(),c.getTipo()});
+		}
+	}
+	
+	private void actualizarTabla() {
+
+		modelo.setRowCount(0);
+
+		cargarCartas();
 	}
 }
